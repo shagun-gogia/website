4f25cd08ba7e1019ad6cf58ab98f760a4ecaad3d
Sarakshi20 <101385350+Sarakshi20@users.noreply.github.com>
2022-05-24 16:57:31 +0530

    Delete script-Proplacement.js

--- script-Proplacement.js
+++
@@ -1,9 +0,0 @@
-if(sessionStorage.name == undefined){
-    document.getElementById("demo").innerText = ""
-    document.getElementById("change-txt").innerText = "Log in"
-}else{
-    document.getElementById("demo").innerText = sessionStorage.name;
-    document.getElementById("change-txt").innerText = "Log out"
-}
-
-
